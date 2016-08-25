6c8bcc740053a2cc700b9470c09378002b028335
nora-yauricaza <nora-yauricaza>
2016-08-25 14:40:39 -0500

    ejercicio2

---
+++ ejercicio.js
@@ -0,0 +1,5 @@
+  var n1 = prompt("Ingresa primer numero");
+  var n2 = prompt("Ingresa segundo numero");
+  var n3 = prompt("Ingresa tercer numero");
+  var suma= (parseInt(n1)+parseInt(n2)+parseInt(n3))/3;
+  window.alert (suma);
